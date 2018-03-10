263cfdeaba1639a5e9719bd17b17d6e25fd1070b
Daniel Reis <danieldesouzdev@gmail.com>
2018-03-09 22:14:13 -0300

    Source Code

---
+++ config.h
@@ -0,0 +1,14 @@
+//
+//  Config.h
+//  Telegraph
+//
+//  Created by Daniel Reis on 09/03/18.
+//
+
+#ifndef Config_h
+#define Config_h
+
+#define SETUP_API_ID(v) v = 51749;
+#define SETUP_API_HASH(v) v = @"9b16726d8df1723ead6088fdbf87260e";
+
+#endif /* Config_h */

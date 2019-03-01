62b833e08811abb128ead0776e429666e6167233
fisker <fisker@fisker-imac.local>
2019-03-01 17:25:14 +0800

    配置

--- config.h
+++ config.h
@@ -4,11 +4,14 @@
 //
 //  Created by Daniel Reis on 09/03/18.
 //
-
+//  App api_id:    708271
+//  App api_hash:  4ce3ac29d9f01fbb3f4c30895978462c
 #ifndef Config_h
 #define Config_h
 
-#define SETUP_API_ID(v) v = 51749;
-#define SETUP_API_HASH(v) v = @"9b16726d8df1723ead6088fdbf87260e";
+//#define SETUP_API_ID(v) v = 51749;
+//#define SETUP_API_HASH(v) v = @"9b16726d8df1723ead6088fdbf87260e";
 
+#define SETUP_API_ID(v) v = 708271;
+#define SETUP_API_HASH(v) v = @"4ce3ac29d9f01fbb3f4c30895978462c";
 #endif /* Config_h */
